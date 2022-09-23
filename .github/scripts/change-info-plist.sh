b21dfb85d60ec29c3f3bba2867745e640d82be19
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-23 11:29:26 +0530

    Update change-info-plist.sh

--- .github/scripts/change-info-plist.sh
+++ .github/scripts/change-info-plist.sh
@@ -3,14 +3,18 @@
 #!/bin/zsh
 set -e
 APP_NAME=$1
-
+myversion=ABDUL
 # *** CHANGE DIRECTORY TO THE GLOBAL PATH ***
-
+echo ${PWD}
+echo "NAME _+_>${myversion}"
+  cd /Users/CS012/Abdul/ReactNative/Practice/demoAPP/ios/demoAPP
+echo "\n aftedr ${PWD}"
     echo "CHANGE IN INFO.PLIST SCRIPT START ==>"
-    ls
-    echo "plutil -p file.plist"
-    ls
-    plutil -replace CFBundleDisplayName -string $APP_NAME Info.plist
+    # ls
+     /usr/libexec/PlistBuddy -c "Set CFBundleDisplayName $APP_NAME" Info.plist
+
+    # sed -i "" -e 's+<key>CFBundleDisplayName.*</key>+<key>CFBundleDisplayName>'${APP_NAME}'</key>+g' Info.plist
+    # sed -i "" -e 's+<string name="app_name">.*</string>+<string name="app_name">'${APP_NAME}'</string>+g' strings.xml
     # plutil -replace CFBundleDisplayName -string $APP_NAME Info.plist
     # cd /android/app/src/main/res/values 
     # ls
