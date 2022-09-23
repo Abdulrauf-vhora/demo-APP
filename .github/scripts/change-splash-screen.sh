4ea28bfe50a2ec03d9ffb23e023f4bd7aa78a61d
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-23 18:57:41 +0530

    splash screen script updated.

--- .github/scripts/change-splash-screen.sh
+++ .github/scripts/change-splash-screen.sh
@@ -2,17 +2,18 @@
 set -e
 # change-splash-screen.sh
 ls
+LOGO_PATH= cd assets/splash.jpg
+ls
 # PROJECT_PATH=$1
 # cd $PROJECT_PATH
 # *** CLEARING OLD EXISITING BUILDS ***
-LOGO_PATH=/Users/CS012/Abdul/ReactNative/Practice/demoApp/assets/logo.png
+
 # watchman watch-del-all
 echo "CHANGE SPLASH SCREEN PROCESS START ==>"
-cd /Users/CS012/Abdul/ReactNative/Practice/demoApp/
 ls
 echo "logo path $LOGO_PATH"
 yarn react-native generate-bootsplash $LOGO_PATH --background-color=F5FCFF
-yarn react-native generate-bootsplash $LOGO_PATH
+ls
 
 echo "<== CHANGE SPLASH SCREEN PROCESS END ==>"
 
