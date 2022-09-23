9e6f7f4939a6590449a4045055983262c3a9a10b
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-23 19:10:14 +0530

    Update change-splash-screen.sh

--- .github/scripts/change-splash-screen.sh
+++ .github/scripts/change-splash-screen.sh
@@ -2,9 +2,15 @@
 set -e
 # change-splash-screen.sh
 ls
-echo "@@@@@@@ ==>"
+echo "@@@@@@@ 11==>"
 
 cd assets
+ls 
+echo "@@@@@@@ 22 ==>"
+
+cd assets/splash.jpg
+echo "@@@@@@@ 22 ==>"
+# ls
 # LOGO_PATH= cd assets/splash.jpg
 ls
 # PROJECT_PATH=$1
@@ -12,11 +18,11 @@ ls
 # *** CLEARING OLD EXISITING BUILDS ***
 
 # watchman watch-del-all
-echo "CHANGE SPLASH SCREEN PROCESS START ==>"
-ls
-# echo "logo path $LOGO_PATH"
-# yarn react-native generate-bootsplash /splash.jpg --background-color=F5FCFF
+# echo "CHANGE SPLASH SCREEN PROCESS START ==>"
 # ls
+# # echo "logo path $LOGO_PATH"
+# yarn react-native generate-bootsplash /splash.jpg --background-color=F5FCFF
+# # ls
 
 echo "<== CHANGE SPLASH SCREEN PROCESS END ==>"
 
