8ccbf49775d4ca5f74a0cc636e7936ad5251a6d5
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-26 15:12:16 +0530

    Update set-app-icon.sh

--- .github/scripts/set-app-icon.sh
+++ .github/scripts/set-app-icon.sh
@@ -4,22 +4,22 @@ echo "<== CHANGE APP ICON PROCESS START"
 echo "<== ls 0.1==>"
 
 ls
-demo='/assests'
+demo='/assests/appLogo.png'
 ls
 echo "only path==> $demo"
-# cd ..
-# yarn global add icon-set-creator
+
+yarn global add icon-set-creator
 # echo "cd "$(dirname "$0")""
 # echo "<== ls 1==>"
 # ls
 #   #  INFO:CHANGE APP ICON PROCESS START
 
 # echo "path==> $path/appLogo.png"
-# iconset create "$path/appLogo.png"
+iconset create $demo
 ls
 
 # ls
-# echo "<== CHANGE APP ICON PROCESS END"
+echo "<== CHANGE APP ICON PROCESS END"
 # ls
   #  INFO:CHANGE APP ICON PROCESS END
 
