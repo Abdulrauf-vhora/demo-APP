2286a7f212aed55aaaa61fd1928b7f4afc358ba2
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-26 14:40:14 +0530

    Update set-app-icon.sh

--- .github/scripts/set-app-icon.sh
+++ .github/scripts/set-app-icon.sh
@@ -8,11 +8,11 @@ echo "<== ls 1==>"
 ls
   #  INFO:CHANGE APP ICON PROCESS START
 
-cd assets
-iconset create appLogo.png
-cd ..
 echo "<== ls 2==>"
-ls
+cd assets 
+# iconset create 
+cd ..
+# ls
 # echo "<== CHANGE APP ICON PROCESS END"
 # ls
   #  INFO:CHANGE APP ICON PROCESS END
