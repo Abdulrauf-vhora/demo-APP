9b61724c76f53bfac2a42f99a50e3d5d8b2df35a
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-26 13:04:02 +0530

    update rename project.yml

--- .github/scripts/change-splash-screen.sh
+++ .github/scripts/change-splash-screen.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 set -e
-# change-splash-screen.sh
 ls
-echo "@@@@@@@ ls 11==>"
-
 # LOGO_PATH=cd assets/splash.jpg
 cd assets
-echo "@@@@@@@ ls 22==>"
-ls
-# PROJECT_PATH=$1
-# cd $PROJECT_PATH
-# *** CLEARING OLD EXISITING BUILDS ***
 
-# watchman watch-del-all
-# echo "CHANGE SPLASH SCREEN PROCESS START ==>"
+# INFO "CHANGE SPLASH SCREEN PROCESS START ==>"
 # ls
 yarn react-native generate-bootsplash splash.jpg --background-color=F5FCFF
 echo "@@@@@@@ ls 33==>"
 ls
-
-cd ..
-echo "@@@@@@@ ls 44==>"
-
-ls
-
-# # ls
-
-echo "<== CHANGE SPLASH SCREEN PROCESS END ==>"
+# INFO "CHANGE SPLASH SCREEN PROCESS END ==>"
 
