a5b01346dd8dd2b31e787a2c22429827b5f4f1aa
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-26 09:49:31 +0530

    Update change-splash-screen.sh

--- .github/scripts/change-splash-screen.sh
+++ .github/scripts/change-splash-screen.sh
@@ -2,11 +2,11 @@
 set -e
 # change-splash-screen.sh
 ls
-echo "@@@@@@@ 11==>"
+echo "@@@@@@@ ls 11==>"
 
 # LOGO_PATH=cd assets/splash.jpg
 cd assets
-echo "@@@@@@@ 22==>"
+echo "@@@@@@@ ls 22==>"
 ls
 # PROJECT_PATH=$1
 # cd $PROJECT_PATH
@@ -15,8 +15,15 @@ ls
 # watchman watch-del-all
 # echo "CHANGE SPLASH SCREEN PROCESS START ==>"
 # ls
-# echo "logo path ${yarn react-native generate-bootsplash /splash.jpg --background-color=F5FCFFH}"
 yarn react-native generate-bootsplash /splash.jpg --background-color=F5FCFF
+echo "@@@@@@@ ls 33==>"
+ls
+
+cd ../../
+echo "@@@@@@@ ls 44==>"
+
+ls
+
 # # ls
 
 echo "<== CHANGE SPLASH SCREEN PROCESS END ==>"
