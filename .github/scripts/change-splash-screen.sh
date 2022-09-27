6dbe1ce090fa9b6beb271e244b08f45883a7517d
abdul <abdulrauf_vhora@codenticsoftware.com>
2022-09-27 12:05:37 +0530

    Update change-splash-screen.sh

--- .github/scripts/change-splash-screen.sh
+++ .github/scripts/change-splash-screen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "<== CHANGE SPLASH SCREEN PROCESS START"
-SPLASH_IMAGE_PATH=$1
-# SPLASH_IMAGE_PATH='assets/splashImage.jpeg'
+# SPLASH_IMAGE_PATH=$1
+SPLASH_IMAGE_PATH='assets/splashImage.jpeg'
 echo "<== SPLASH IMAGE PATH==> $SPLASH_IMAGE_PATH"
 
 # INFO "CHANGE SPLASH SCREEN PROCESS START ==>"
